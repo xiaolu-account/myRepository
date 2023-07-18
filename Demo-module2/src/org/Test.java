35f226e3309edb77438bdce683c653e65aa1abdf
xiaolu-account <xiaolu-account@qq.com>
2023-07-18 20:44:58 +0800

    第二次更新

---
+++ Demo-module2/src/org/Test.java
@@ -0,0 +1,11 @@
+package org;
+
+public class Test {
+    private String name;
+    private Integer age;
+    private String sex;
+
+    public void test(){
+        System.out.println("这是一个测试方法");
+    }
+}
